8c9c0c0cd0d116d3dc1fa654ebda9758435b382d
JakeZehring <jzehring@mail.csuchico.edu>
2015-10-20 12:19:05 -0700

    finshed basic views  for Topic

--- wbib/db/seeds.rb
+++ wbib/db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+topics = Topic.create(
+  [
+     { title: "Puppies", description: "Who doesn't love puppies?" },
+     { title: "Cake", description: "Delicious" },
+     { title: "chocolate", description: "Also cake" }
+  ]
+)
+
+refrences = Reference.create(
+  [
+     { URL: "www.puppies.net", topic_id: topics[0].id }
+
+  ]
+)
