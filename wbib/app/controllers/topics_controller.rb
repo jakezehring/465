ab3157f407ecf121ecb9713e327a80bcf92aa497
JakeZehring <jzehring@mail.csuchico.edu>
2015-10-20 12:19:19 -0700

    finshed basic views  for Topic

---
+++ wbib/app/controllers/topics_controller.rb
@@ -0,0 +1,47 @@
+class TopicsController < ApplicationController
+	before_action :set_topic, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@topic = Topic.all
+	end
+
+	def show
+	end
+
+	def new
+		@topic = Topic.new
+	end
+
+	def create
+		@topic = Topic.new(params.require(:topic).permit(:title, :description))
+		
+		if @topic.save
+			redirect_to @topic
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @topic.update(params.require(:topic).permit(:title, :description))
+			redirect_to @topic
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@topic.destroy
+		redirect_to topics_url
+	end
+
+	private
+
+	def set_topic
+		@topic = Topic.find(params[:id])
+	end
+
+end
